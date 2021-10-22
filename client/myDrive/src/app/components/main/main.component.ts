3cf971088e2c88978ab822ffac05a8c6c64bc6bf
Joejn <jonas.neuhauser@gmx.at>
2021-10-22 20:39:02 +0200

    added function to toggle sidenav-width and content

--- client/myDrive/src/app/components/main/main.component.ts
+++ client/myDrive/src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-main',
@@ -8,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   public currentTab: string = "home"
+  public isSidenavExpanded = true
+  public isManuelSidenavExpanded = false
+  public expansionIcon = "arrow_back"
+
+  @ViewChild("sidenav") sidenav!: HTMLElement
 
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener("resize", () => {
+      if (this.isManuelSidenavExpanded) {
+        return
+      }
+
+      console.log(this.isManuelSidenavExpanded)
+      this.setSidenaveExpanded(window.innerWidth > 768)
+    })
+  }
+
+  onSidenavExpandClicked() {
+    this.isManuelSidenavExpanded = true
+    this.setSidenaveExpanded(!this.isSidenavExpanded)
+  }
+
+  setSidenaveExpanded(isExpanded: boolean) {
+    this.isSidenavExpanded = isExpanded
+    const sidenav = document.getElementById("sidenav")
+    if (isExpanded) {
+      sidenav?.classList.remove("sidenav--collapsed")
+      this.expansionIcon = "arrow_back"
+    } else {
+      sidenav?.classList.add("sidenav--collapsed")
+      this.expansionIcon = "arrow_forward"
+    }
   }
 
 }
